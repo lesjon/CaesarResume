548ecfd2caee655fd5cd0305da4e5fd1dd6cf4d7
Leon Klute <l.klute@caesar.nl>
2023-05-23 16:15:35 +0200

    main: clean up logging

--- upload.py
+++ upload.py
@@ -1,10 +1,7 @@
 import boto3
 
 def save_file(file_name, data):
-    print(f'{file_name=},{data=}')
     s3 = boto3.client('s3')
-    print(f'{s3=}')
     bucket_name = "resume.leonklute.nl"
-
-    result = s3.put_object(Bucket=bucket_name, Key=file_name, Body=data)
-    print(f'{result=}')
+    s3.put_object(Bucket=bucket_name, Key=file_name, Body=data)
+    print(f"saved file: {file_name} to {bucket_name}")
